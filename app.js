d6c45b67e27048d99377164fd8cbd0e025ca28cb
karanwadhwa <karanwadhwa0309@gmail.com>
2020-08-26 17:17:43 +0530

    INITIAL COMMIT

---
+++ app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+
+$('.flag').click(function(){
+    $('.collapse').collapse('hide')
+    
+})
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
+
+})
